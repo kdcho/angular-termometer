7a47dcaa30270b57dd09fa97e226bb0943b5ac32
Krystian Chodak <16298695+kdcho@users.noreply.github.com>
2024-02-11 00:15:09 +0100

    Add template to termometer component - Add basic html structure with classes - Add css to arrange html elements in such a way that they show a thermometer with the current temp and min an max

--- thermometer/src/app/app.component.ts
+++ thermometer/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ThermometerComponent } from './components/thermometer/thermometer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ThermometerComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    imports: [ThermometerComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'thermometer';
+    title = 'thermometer';
 }
