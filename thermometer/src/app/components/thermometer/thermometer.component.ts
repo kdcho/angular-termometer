d8b08b976601702e5438982ac86dfb6257684885
Krystian Chodak <16298695+kdcho@users.noreply.github.com>
2024-02-11 14:26:05 +0100

    Add a method to calculate the angle for the thermometer

--- thermometer/src/app/components/thermometer/thermometer.component.ts
+++ thermometer/src/app/components/thermometer/thermometer.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-thermometer',
-  standalone: true,
-  imports: [],
-  templateUrl: './thermometer.component.html',
-  styleUrl: './thermometer.component.css'
+    selector: 'app-thermometer',
+    standalone: true,
+    imports: [],
+    templateUrl: './thermometer.component.html',
+    styleUrl: './thermometer.component.css',
 })
 export class ThermometerComponent {
+    minTemp: number = 10;
+    maxTemp: number = 2000;
+    currentTemp: number = 2000;
 
+    getAngle(): number {
+        let minAngle: number = -222;
+        let maxAngle: number = 41;
+
+        let angle =
+            minAngle +
+            (maxAngle - minAngle) *
+                ((this.currentTemp - this.minTemp) /
+                    (this.maxTemp - this.minTemp));
+
+        return angle;
+    }
 }
